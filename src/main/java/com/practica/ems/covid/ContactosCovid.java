62c0fd14a4db26967eedb65a939110577478a83f
sofialuna.gonzalez <sofialuna.gonzalez>
2024-04-11 18:20:40 +0200

    #17

--- src/main/java/com/practica/ems/covid/ContactosCovid.java
+++ src/main/java/com/practica/ems/covid/ContactosCovid.java
@@ -97,11 +97,11 @@ public class ContactosCovid {
 	}
 
 	@SuppressWarnings("resource")
-	public void loadDataFile(String fichero, boolean reset, File archivo, FileReader fr, BufferedReader br, String datas[], String data ) {
+	public void loadDataFile(String fichero, boolean reset, File archivo, String datas[], String data ) {
 		try {
 			archivo = new File(fichero);
-			fr = new FileReader(archivo);
-			br = new BufferedReader(fr);
+			FileReader fr = new FileReader(archivo);
+			BufferedReader br = new BufferedReader(fr);
 			if (reset) {
 				this.poblacion = new Poblacion();
 				this.localizacion = new Localizacion();
